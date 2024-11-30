969053b3f62fc350cbcf6733c0903baf05c8e865
renowan <renowan@yahoo.co.jp>
2024-12-01 06:29:20 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import FccButton from "./components/FccButton.vue";
 import ZButton from "./components/Z/ZButton.vue";
 import ZAlert from "./components/Z/ZAlert.vue";
-import * as utils from "./script/utils";
+// import * as utils from "./script/utils";
 
-export { FccButton, ZButton, ZAlert, utils };
+export { FccButton, ZButton, ZAlert };
+
+export * from "./script/utils";
