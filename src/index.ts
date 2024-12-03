6c9cc7c43aadebde85b903e7bb594142ac502623
renowan <renowan@yahoo.co.jp>
2024-12-03 21:46:05 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import FccButton from "./components/FccButton.vue";
 import ZButton from "./components/Z/ZButton.vue";
 import ZAlert from "./components/Z/ZAlert.vue";
 import * as utils from "./script/utils";
-import { zuiTokens } from "./contants/tailwindcssConfig";
+import { zuiTokens } from "./contants/zuiTokens";
 
 export { FccButton, ZButton, ZAlert, utils, zuiTokens };
