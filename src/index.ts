c0be693dab0a6fb6a51555775b81e9fa964303e9
renowan <renowan@yahoo.co.jp>
2024-12-05 08:51:20 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ZButton from "./components/Z/ZButton.vue";
 import ZAlert from "./components/Z/ZAlert.vue";
+import ZIcon from "./components/Z/ZIcon.vue";
 import * as utils from "./components/Z/utils";
 import { zuiTokens } from "./contants/zuiTokens";
 
@@ -9,5 +10,6 @@ export default {
   install: (app) => {
     app.component("ZButton", ZButton);
     app.component("ZAlert", ZAlert);
+    app.component("ZIcon", ZIcon);
   },
 };
