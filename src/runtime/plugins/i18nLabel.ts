d9440e2cc10043c2f082add4b8bd497717e3f340
renowan <renowan@yahoo.co.jp>
2024-12-21 07:45:30 +0900

    test

--- src/runtime/plugins/i18nLabel.ts
+++ src/runtime/plugins/i18nLabel.ts
@@ -5,8 +5,11 @@ export default defineNuxtPlugin((nuxtApp) => {
   return {
     provide: {
       i18nLabel: (defaults: Record<string, any>) => {
+        console.log('i18nLabel')
         const ZFormLabel = nuxtApp.vueApp._context.components.ZFormLabel as Component
+        console.log('ZFormLabel', ZFormLabel)
         if (ZFormLabel && ZFormLabel.props) {
+          console.log('if')
           // 既存のデフォルト値を保持しながら新しい値で上書き
           Object.keys(defaults).forEach(key => {
             if (ZFormLabel.props[key] && ZFormLabel.props[key].default !== undefined) {
